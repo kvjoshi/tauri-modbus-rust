86adeee6926e32fc0a2d27f64e61dd60294211ab
Karmavir Joshi <karmavirj@live.com>
2023-06-11 20:08:36 +0530

    init tauri with vite and react

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,26 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+extern crate modbus;
+//importing and using modbus
+use modbus::{Client,Coil};
+use modbus::tcp;
+use serde_json::Value::Bool;
+
+
+#[tauri::command]
+
+
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
